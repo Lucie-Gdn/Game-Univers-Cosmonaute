97d650e701792acc7faf94cf4865eab67a071fc0
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-02-23 12:13:02 +0100

    feu timer ok

--- cosmosadventures/rocket_mvmt.lua
+++ cosmosadventures/rocket_mvmt.lua
@@ -1,22 +1,28 @@
 function create_rocket_fire()
 rocket_fire={
-    sprite = 19,
+    sprite = 17,
     x = 2,
-    y = 5
+    y = 5,  
 }
+rocket_fire_anim_time = 0
+rocket_fire_anim_wait = 0.08
 end 
+
+function update_rocket_fire()
+    if time() - rocket_fire_anim_time > rocket_fire_anim_wait then
+        rocket_fire.sprite +=2
+        rocket_fire_anim_time=time()
+
+        if rocket_fire.sprite > 26 then 
+        rocket_fire.sprite=19
+        end
+    
+    end 
+   
+end
 
 function draw_rocket_fire()
     spr(rocket_fire.sprite,rocket_fire.x*8,rocket_fire.y*8,2,2)
 end 
-
-function update_rocket_fire()
-    rocket_fire.sprite +=2
-    if rocket_fire.sprite > 27 then 
-        rocket_fire.sprite=19
-    end
-end
-
-
 
 
